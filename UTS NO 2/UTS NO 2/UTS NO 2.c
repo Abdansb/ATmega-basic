2ca537ad141f8b31704c9e699ff535813faf5659
Abdan Subekti <abdanxcx@gmail.com>
2022-10-27 11:00:52 +0700

    Fix UTS gess

--- UTS NO 2/UTS NO 2/UTS NO 2.c
+++ UTS NO 2/UTS NO 2/UTS NO 2.c
@@ -7,54 +7,44 @@
 #define F_CPU 16000000L
 #include <avr/io.h>
 #include <util/delay.h>
-#define RES 500
 
-void adc_init()
-{
-	// Tegangan referensi AREF=AVCC kapasitor eksternal
-	ADMUX = (1<<REFS0);
-	// ADC enable ADEN bit 7
-	ADCSRA |= (1 << 7); 
-	// ADPS prescaler 128, 16000000/128 = 125000
-	ADCSRA |= (1 <<2)|(1 << 1)|(1 << 0); 
+int main(void)
+{	
+	// Variabel konversi biner desimal
+	int nilaiBawah;
+	int nilaiAtas;
+	int nilaiSensor;
+
+	DDRD = 0xff;    // PORTD ouput
+	PORTD=0x00;		// Pull LOW
+	
+	ADCSRA |= (1 << 7);						// ADC enable ADEN bit 7
+	ADCSRA |= (1 <<2)|(1 << 1)|(1 << 0);	// ADPS prescaler 128, 16000000/128 = 125000
+	ADMUX = (1<<REFS0);						// Tegangan referensi AREF=AVCC + kapasitor eksternal
+
+	// Single ended input ADC, Analog channel & gain selection bit (MUX0)
+	ADMUX |= (1 << 0); 
 	// Mode free running, bit 7,6,5 = 0
-	SFIOR &= ~(1 << 7) &~ (1 << 6) &~ (1 << 5); 
-	
-}
+	SFIOR &= ~(1 << 7) &~ (1 << 6) &~ (1 << 5);
 
-// Fungsi baca ADC
-uint16_t adc_read(uint8_t ch)
-{
-	ch &= 0b00000111; 
-	ADMUX = (ADMUX & 0xF8)|ch;   
-	ADCSRA |= (1<<ADSC);
-	while(ADCSRA & (1<<ADSC));
-	
-	return (ADC);
-}
-
-int main()
-{
-	uint16_t adc_result0, adc_result1;
-	char int_buffer[10];
-	DDRC = 0x01;
-	
-
-	adc_init();
-	
-
-	
-	_delay_ms(50);
-	
 	while(1)
 	{
-		adc_result0 = adc_read(0);     
+		ADCSRA |= (1 << 6); // ADSC, ADC start conversion, free running 25 clock
 
-		if (adc_result0 < RES)
-		PORTC = 0x01;
-		else
-		PORTC = 0x00;
+		if (ADCSRA & (1 << 6)) // Tunggu hingga ADC selesai
+		{
+			nilaiBawah = (int)ADCL;
+			nilaiAtas = (int)ADCH*256;
+			nilaiSensor = nilaiAtas+nilaiBawah;
 		
-		
+				if (nilaiSensor>768)
+					{	PORTD = 0x01;	}
+				else if (nilaiSensor>511)
+					{	PORTD = 0x02;	}	
+				else if (nilaiSensor>255)
+					{	PORTD = 0x04;	}
+				else if (nilaiSensor>1)
+					{	PORTD = 0x08;	}       
+		}      
 	}
 }
